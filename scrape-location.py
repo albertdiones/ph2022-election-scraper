93b0ba03312da2289f151fcded949ed888736a7a
Albert Diones <albert.diones@flexisourceit.com.au>
2022-05-13 07:35:36 +0800

    Don't fetch result option

--- scrape-location.py
+++ scrape-location.py
@@ -8,9 +8,9 @@ from time import sleep
 import json
 import csv
 from random import randint
+import sys
 
-def getJson(urlFormat, id, cacheDir):
-    shard = str(id)[0:2]
+def getJson(urlFormat, shard, id, cacheDir, fetch = True):
 
     fileName = cacheDir + '/' + str(id) + '.json';
     #print("Sleep time: " + str(randomSleepTime))
@@ -19,9 +19,11 @@ def getJson(urlFormat, id, cacheDir):
 
     if exists(fileName):
         file = open(fileName,'r')
-        responseText = content = file.read()
+        responseText = file.read()
 
     if responseText == "":
+        if not fetch:
+            return False
 
         randomSleepTime = randint(10,200)/1000;
         url = urlFormat.format(str(shard), str(id))
@@ -38,6 +40,7 @@ def getJson(urlFormat, id, cacheDir):
         except Exception as error:
             print("Error: {0}".format(error))
             sleep(20)
+            return False
 
         file = open(fileName,'w+')
         responseText = response.text
@@ -49,35 +52,98 @@ def getJson(urlFormat, id, cacheDir):
     except Exception as error:
         print(responseText)
         print("Error: {0}".format(error))
-        return False;
+        return False
 
     return data;
 
+def getLocationData(locationId):
+    shard = str(locationId)[0:2]
+    return getJson('https://2022electionresults.comelec.gov.ph/data/regions/{0}/{1}.json', shard, int(locationId), 'region-responses')
 
-phData = getJson('https://2022electionresults.comelec.gov.ph/data/regions/{0}/{1}.json',44021, 'region-responses')
+def getResultData(presinctId, fetch = True):
+    shard = str(presinctId)[0:3]
+    return getJson('https://2022electionresults.comelec.gov.ph/data/results/{0}/{1}.json', shard, int(presinctId), 'responses', fetch)
+
+
+phData = getLocationData("44021")
 
 if (phData == False):
     print("Data not found")
     exit()
 
+
+# includes the following candidate ids (currently BBM and Leni)
+includeCandidateIds = [ 46444,46447 ]
+
+fetchResults =  False if "--dont-fetch-results" in sys.argv else True
+
+
+csvFile = open('location-results.csv','w')
+csvWriter = csv.writer(csvFile)
+
+csvWriter.writerow(["BBM\nVotes","BBM %","Leni\nVotes","Leni%","Presincts","Poll Place","Barangay","City","Province"])
+
+
 for regionId in phData["srs"]:
     region = phData["srs"][regionId]
-    regionData = getJson('https://2022electionresults.comelec.gov.ph/data/regions/{0}/{1}.json',int(region["rc"]), 'region-responses')
+    regionData = getLocationData(region["rc"])
 
     for provinceId in regionData['srs']:
         province = regionData['srs'][provinceId]
-        provinceData = getJson('https://2022electionresults.comelec.gov.ph/data/regions/{0}/{1}.json',int(province["rc"]), 'region-responses')
+        provinceData = getLocationData(province["rc"])
 
         for cityId in provinceData['srs']:
             city = provinceData['srs'][cityId]
-            cityData = getJson('https://2022electionresults.comelec.gov.ph/data/regions/{0}/{1}.json',int(city["rc"]), 'region-responses')
+            cityData = getLocationData(city["rc"])
 
             for barangayId in cityData['srs']:
                 barangay = cityData['srs'][barangayId]
-                barangayData = getJson('https://2022electionresults.comelec.gov.ph/data/regions/{0}/{1}.json',int(barangay["rc"]), 'region-responses')
+                barangayData = getLocationData(barangay["rc"])
 
                 if barangayData == False:
-                    exit();
+                    exit()
 
                 for presinct in barangayData['pps']:
-                    print(str(presinct["ppc"]) + ": " + presinct['ppn'])
+                    
+                    result = getResultData(presinct['ppc'], fetchResults)
+                    
+
+                    candidateVotes = {
+                        includeCandidateIds[0]: 0,
+                        includeCandidateIds[1]: 0
+                    };
+                    candidatePercent = {
+                        includeCandidateIds[0]: 0,
+                        includeCandidateIds[1]: 0
+                    };
+
+                    if result:
+                        for vote in result["rs"]:
+                            candidateId = vote["bo"];
+                            if candidateId in includeCandidateIds:
+                                candidateVotes[candidateId] = vote['v']
+                                candidatePercent[candidateId] = vote['per']
+
+                    presincts = []
+                    # not sure what VB means
+                    for vbs in presinct['vbs']:
+                        presincts.append(vbs['cpre'])
+                    
+                    csvRow = [
+                        candidateVotes[includeCandidateIds[0]],
+                        candidatePercent[includeCandidateIds[0]],
+                        candidateVotes[includeCandidateIds[1]],
+                        candidatePercent[includeCandidateIds[1]],
+                        ', '.join(presincts),
+                        presinct["ppn"],
+                        barangayData['rn'],
+                        cityData['rn'],
+                        provinceData['rn'],
+                        regionData['rn']
+                    ]
+                    print(csvRow)
+
+                    csvWriter.writerow(csvRow)
+
+
+csvFile.close()
